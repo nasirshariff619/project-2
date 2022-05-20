e5d732240df6609bd97ed6abe84f7f0409a7919b
nasirshariff619 <nasirshariff97@gmail.com>
2022-05-20 13:39:22 +0000

    adding docker-compose.yaml and ansible playbooks

--- S1/app.py
+++ S1/app.py
@@ -4,10 +4,10 @@ import requests
 app = Flask(__name__)
 @app.route('/country/food', methods = ['GET', 'POST'])
 def get_task():
-    country = requests.get('service2:5001/get/country').text #local host can be container name
-    food = requests.get('service3:5002/get/food').text
+    country = requests.get('http://service2:5001/get/country').text #local host can be container name
+    food = requests.get('http://service3:5002/get/food').text
     country_food = country + " " + food 
-    task = requests.post('service4:5003/post/task', data=country_food).text
+    task = requests.post('http://service4:5003/post/task', data=country_food).text
     
     return render_template('home.html', title = 'Country Food', country = country, food = food, task = task)
 
--- S3/app.py
+++ S3/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route('/get/food', methods = ['GET'])
 def get_food():
     food = ['Sausage-roll', 'Croissant', 'Schnitzel']
+    #food = ['Sausage-roll', 'Croissant']
     x = random.randint(0,2)
     return Response(food[x], mimetype = 'text/plain')  
 if __name__ == '__main__':
